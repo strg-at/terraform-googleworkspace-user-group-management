9becae27bc1ff2c2a326a09273eb039607da0fdf
Jasmin Müller <jasmin.mueller@strg.at>
2021-05-17 22:02:15 +0200

    Change: renamed ressource in user_to_groups for better naming INPRO-406

--- modules/users_to_groups/users_to_groups.tf
+++ modules/users_to_groups/users_to_groups.tf
@@ -10,7 +10,7 @@ terraform {
 /**
  * Ressource documentation: https://registry.terraform.io/providers/DeviaVir/gsuite/latest/docs/resources/group_member
  */
-resource "gsuite_group_member" "member" {
+resource "gsuite_group_member" "group" {
   for_each = toset(var.user.roles)
   group    = var.groups[each.value].email
   email    = var.user.primary_email
