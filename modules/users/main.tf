c47771f5e9be90ec2fbcf2b56ba97f9c7954d9f9
Jasmin Müller <jasmin.mueller@strg.at>
2021-05-18 18:00:59 +0200

    New: add user alias support INPRO-416

--- modules/users/main.tf
+++ modules/users/main.tf
@@ -20,7 +20,9 @@ resource "gsuite_user" "user" {
   org_unit_path     = var.user.org_unit_path
   is_suspended      = var.user.is_suspended
   suspension_reason = var.user.suspension_reason
-  update_existing   = true # If omitted or `true` existing GSuite users defined as Terraform resources will be imported by `terraform apply`.
+  aliases           = var.user.aliases
+
+  update_existing = true # If omitted or `true` existing GSuite users defined as Terraform resources will be imported by `terraform apply`.
   lifecycle {
     ignore_changes = [
       recovery_email,
